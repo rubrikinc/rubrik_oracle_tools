477e138c194640d924c789eefed90296525954ca
pcrouleur <vendors@zgoda.com>
2020-01-05 16:20:52 -0700

    Changes for new setup.

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup
 setup(
     name='rubrikOracleTools',
     version='1.0',
-    py_modules=['oracle_backup_info', 'oracle_mount', 'oracle_unmount'],
+    py_modules=['rubrik_oracle_module', 'rubrik_oracle_backup_info', 'rubrik_oracle_backup_mount', 'rubrik_oracle_unmount', 'rubrik_oracle_db_mount'],
     install_requires=[
-        'requests',
+        'requests >= 2.18.4, != 2.22.0',
         'rubrik_cdm',
-        'urllib3',
+        'urllib3 <1.25, >=1.21.1',
         'Click',
         'pytz',
     ],
     entry_points='''
         [console_scripts]
-        oracle_backup_info=oracle_backup_info:cli
-        oracle_mount=oracle_mount:cli
-        oracle_unmount=oracle_unmount:cli
+        rubrik_oracle_backup_info=rubrik_oracle_backup_info:cli
+        rubrik_oracle_backup_mount=rubrik_oracle_backup_mount:cli
+        rubrik_oracle_db_mount=rubrik_oracle_db_mount:cli
+        rubrik_oracle_unmount=rubrik_oracle_unmount:cli
     '''
 )
