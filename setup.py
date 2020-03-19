e5b9f0e92aa629a40f3c5b294872af10ab28df46
pcrouleur <vendors@zgoda.com>
2020-03-18 18:54:57 -0600

    Added clone modules.

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
     version='1.0',
     py_modules=['rubrik_oracle_module', 'rubrik_oracle_backup_info', 'rubrik_oracle_backup_mount',
                 'rubrik_oracle_unmount', 'rubrik_oracle_db_mount', 'rubrik_oracle_snapshot'
-                'rubrik_oracle_log_backup'],
+                'rubrik_oracle_log_backup', 'rubrik_oracle_db_clone_mount', 'rubrik_oracle_db_clone_unmount'],
     install_requires=[
         'requests >= 2.18.4, != 2.22.0',
         'rubrik_cdm',
         'urllib3 <1.25, >=1.21.1',
         'Click',
         'pytz',
+        'yaspin'
     ],
     entry_points='''
         [console_scripts]
@@ -21,5 +22,7 @@ setup(
         rubrik_oracle_unmount=rubrik_oracle_unmount:cli
         rubrik_oracle_snapshot=rubrik_oracle_snapshot:cli
         rubrik_oracle_log_backup=rubrik_oracle_log_backup:cli
+        rubrik_oracle_db_clone_mount=rubrik_oracle_db_clone_mount:cli
+        rubrik_oracle_db_clone_unmount=rubrik_oracle_db_clone_unmount:cli
     '''
 )
