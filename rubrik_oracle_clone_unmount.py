fe52c64f544dce16c5d75601870d7c3a814e6c2b
Julian Zgoda <julian.zgoda@rubrik.com>
2020-03-30 14:16:21 -0700

    Made required options mandatory.

--- rubrik_oracle_clone_unmount.py
+++ rubrik_oracle_clone_unmount.py
@@ -8,8 +8,8 @@ import datetime
 
 @click.command()
 @click.argument('host_cluster_db')
-@click.option('--new_oracle_name', '-n', type=str, help='Oracle database clone name. If unmounting more than one separate with commas.')
-@click.option('--oracle_home', '-o', type=str, help='ORACLE_HOME path for the database(s)')
+@click.option('--new_oracle_name', '-n', required=True, type=str, help='Oracle database clone name. If unmounting more than one separate with commas.')
+@click.option('--oracle_home', '-o', required=True, type=str, help='ORACLE_HOME path for the mounted database(s)')
 @click.option('--all', '-a', is_flag=True, help='Unmount all mounts from the source host:db. Provide all the clone names separated by commas.')
 def cli(host_cluster_db, new_oracle_name, oracle_home, all):
     """
@@ -19,11 +19,6 @@ def cli(host_cluster_db, new_oracle_name, oracle_home, all):
 \b
     Args:
         host_cluster_db (str): The hostname the database is running on : The source database name
-        new_oracle_name (str): The name the source db was changed to.
-        oracle_home (str): The ORACLE_HOME on the live mount host.
-        all (bool): Unmount all mounts from the source host:db. Provide all the clone names separated by commas.
-\b
-    Returns:
 
     """
     logger = logging.getLogger(__name__)
@@ -41,10 +36,10 @@ def cli(host_cluster_db, new_oracle_name, oracle_home, all):
     utc = pytz.utc
     fmt = '%Y-%m-%d %H:%M:%S %Z'
     if not live_mount_ids:
-        raise RubrikOracleUnmountError("No live mounts found for {} live mounted on {}. ".format(host_cluster_db[1], host_cluster_db[0]))
+        raise RubrikOracleCloneUnmountError("No live mounts found for {} live mounted on {}. ".format(host_cluster_db[1], host_cluster_db[0]))
     else:
         if len(live_mount_ids) == 0:
-            raise RubrikOracleUnmountError(
+            raise RubrikOracleCloneUnmountError(
                 "No live mounts found for {} live mounted on {}. ".format(host_cluster_db[1], host_cluster_db[0]))
         elif len(live_mount_ids) > 1:
             if all:
@@ -63,7 +58,7 @@ def cli(host_cluster_db, new_oracle_name, oracle_home, all):
                     logger.warning("Clone database {} has been dropped.".format(name))
                 return
             else:
-                raise RubrikOracleUnmountError(
+                raise RubrikOracleCloneUnmountError(
                     "More than one backup of {} is live mounted on {}. Use the --all flag to unmount them all.".format(host_cluster_db[1], host_cluster_db[0]))
         else:
             unmount_info = rbk.live_mount_delete(rubrik, live_mount_ids[0], force)
@@ -74,7 +69,7 @@ def cli(host_cluster_db, new_oracle_name, oracle_home, all):
             unmount_info = rbk.async_requests_wait(rubrik, unmount_info['id'], 20)
             logger.info("Async request completed with status: {}".format(unmount_info['status']))
             if unmount_info['status'] != "SUCCEEDED":
-                raise RubrikOracleUnmountError("Unmount of the {} database live mounted on {} did not succeed. Request completed with status {}.".format(host_cluster_db[1], host_cluster_db[0], unmount_info['status']))
+                raise RubrikOracleCloneUnmountError("Unmount of the {} database live mounted on {} did not succeed. Request completed with status {}.".format(host_cluster_db[1], host_cluster_db[0], unmount_info['status']))
             else:
                 logger.warning("Backup files have been unmounted.")
             rbk.oracle_db_clone_cleanup(new_oracle_name[0], oracle_home)
@@ -82,7 +77,7 @@ def cli(host_cluster_db, new_oracle_name, oracle_home, all):
             return
 
 
-class RubrikOracleUnmountError(rbk.NoTraceBackWithLineNumber):
+class RubrikOracleCloneUnmountError(rbk.NoTraceBackWithLineNumber):
     """
         Renames object so error is named with calling script
     """
