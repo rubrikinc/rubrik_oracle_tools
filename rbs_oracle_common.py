186190a9c8bcc8faada2df014d12f747e0ec4601
pcrouleur <vendors@zgoda.com>
2020-04-20 08:34:47 -0600

    Added mount class and additional methods.

--- rbs_oracle_common.py
+++ rbs_oracle_common.py
@@ -4,13 +4,16 @@
 import os
 import logging
 import sys
+import time
 import datetime
 import pytz
 import json
 import subprocess
 from subprocess import PIPE, Popen
 import re
+import glob
 import inspect
+from yaspin import yaspin
 import urllib3
 import rubrik_cdm
 
@@ -32,7 +35,7 @@ class NoTraceBackWithLineNumber(Exception):
         sys.exit(self)
 
 
-class RubrikOracleModuleError(NoTraceBackWithLineNumber):
+class RbsOracleCommonError(NoTraceBackWithLineNumber):
     """
     Renames object so error is named with calling script
     """
@@ -54,8 +57,11 @@ class RubrikConnection:
         self.logger.debug("Instantiating RubrikConnection using rubrik_cdm.Connect.")
         self.connection = rubrik_cdm.Connect(self.config['rubrik_cdm_node_ip'], self.config['rubrik_cdm_username'], self.config['rubrik_cdm_password'], self.config['rubrik_cdm_token'])
         self.cluster = self.connection.get('v1', '/cluster/me')
+        self.name = self.cluster['name']
+        self.cluster_id = self.cluster['id']
         self.timezone = self.cluster['timezone']['timezone']
-        self.logger.info("Connected to cluster: {}, version: {}, Timezone: {}.".format(self.cluster['name'], self.cluster['version'], self.timezone))
+        self.version = self.cluster['version']
+        self.logger.info("Connected to cluster: {}, version: {}, Timezone: {}.".format(self.name, self.version, self.timezone))
 
 
 class RubrikRbsOracleDatabase:
@@ -89,9 +95,9 @@ class RubrikRbsOracleDatabase:
         # Instance names can be stored/entered with and without the domain name so
         # we will compare the hostname without the domain.
         if self.is_ip(self.database_host):
-            raise RubrikOracleModuleError("A hostname is required for the Oracle host, do not use an IP address.")
+            raise RbsOracleCommonError("A hostname is required for the Oracle host, do not use an IP address.")
         if oracle_dbs['total'] == 0:
-            raise RubrikOracleModuleError(
+            raise RbsOracleCommonError(
                 "The {} object '{}' was not found on the Rubrik cluster.".format(self.database_name, self.database_host))
         elif oracle_dbs['total'] > 0:
             for db in oracle_dbs['data']:
@@ -194,12 +200,202 @@ class RubrikRbsOracleDatabase:
         """
         sla_info = self.rubrik.connection.get('v1', '/sla_domain?name={}'.format(sla_name))
         if sla_info['total'] == 0:
-            raise RubrikOracleModuleError("The sla: {} was not found on this Rubrik cluster.".format(sla_name))
+            raise RbsOracleCommonError("The sla: {} was not found on this Rubrik cluster.".format(sla_name))
         elif sla_info['total'] > 1:
-            raise RubrikOracleModuleError("Multiple SLAs with the name {} were found on this cluster.".format(sla_name))
+            raise RbsOracleCommonError("Multiple SLAs with the name {} were found on this cluster.".format(sla_name))
         else:
             sla_id = sla_info['data'][0]['id']
         return sla_id
+
+    # New methods
+    def live_mount(self, host_id, time_ms, files_only=False, mount_path=None):
+        """
+        Live mounts a Rubrik Database backup on the requested host or cluster.
+
+        Args:
+            self (object): Database Object
+            host_id (str):  The Rubrik host or cluster for the mount.
+            time_ms  (str):  The point in time of the backup to mount.
+            files_only (bool):  Mount the backup pieces only.
+            mount_path (str):  The path to mount the files only restore. (Required if files_only is True).
+
+        Returns:
+            live_mount_info (dict): The information about the requested live mount returned from the Rubrik CDM.
+        """
+        payload = {
+            "recoveryPoint": {"timestampMs": time_ms},
+            "targetOracleHostOrRacId": host_id,
+            "targetMountPath": mount_path,
+            "shouldMountFilesOnly": files_only
+        }
+        live_mount_info = self.rubrik.connection.post('internal', '/oracle/db/{}/mount'.format(self.oracle_id), payload)
+        return live_mount_info
+
+    def get_host_id(self, primary_cluster_id, hostname):
+        """
+        Gets the Oracle database host using the hostname.
+
+        Args:
+            self (object): Database Object
+            hostname (str): The oracle host name
+            primary_cluster_id (str): The rubrik cluster id
+
+        Returns:
+            host_id (str): The host id
+        """
+        host_info = self.rubrik.connection.get('internal', '/oracle/host?name={}'.format(hostname))
+        host_id = ''
+        if host_info['total'] == 0:
+            raise RbsOracleCommonError("The host: {} was not found on the Rubrik CDM.".format(hostname))
+        elif host_info['total'] > 1:
+            # found_hosts = []
+            for hosts in host_info['data']:
+                if hosts['primaryClusterId'] == primary_cluster_id and hosts['status'] == 'Connected':
+                    host_id = hosts['id']
+            # raise RubrikOracleModuleError("Multiple Host IDs found: {} ".format(found_hosts))
+        else:
+            host_id = host_info['data'][0]['id']
+        return host_id
+
+    def get_rac_id(self, primary_cluster_id, rac_cluster_name):
+        """
+        Gets the RAC Cluster ID using the cluster name.
+
+        Args:
+            self (object): Database Object
+            rac_cluster_name (str): The RAC cluster name.
+            primary_cluster_id (str): The rubrik cluster id
+
+        Returns:
+            rac_id (str): The RAC Cluster ID  if found otherwise will exit with error condition.
+        """
+        rac_info = self.rubrik.connection.get('internal', '/oracle/rac?name={}'.format(rac_cluster_name))
+        rac_id = ''
+        if rac_info['total'] == 0:
+            raise RbsOracleCommonError(
+                "The target: {} either was not found or is not a RAC cluster.".format(rac_cluster_name))
+        elif rac_info['total'] > 1:
+            for rac in rac_info['data']:
+                if rac['primaryClusterId'] == primary_cluster_id and rac['status'] == 'Connected':
+                    rac_id = rac['id']
+                    break
+            # raise RubrikOracleModuleError("Multiple RAC IDs found: {} ".format(found_clusters))
+        else:
+            rac_id = rac_info['data'][0]['id']
+        return rac_id
+
+    def async_requests_wait(self, requests_id, timeout):
+        timeout_start = time.time()
+        terminal_states = ['FAILED', 'CANCELED', 'SUCCEEDED']
+        while time.time() < timeout_start + (timeout * 60):
+            oracle_request = self.rubrik.connection.get('internal', '/oracle/request/{}'.format(requests_id), timeout=60)
+            if oracle_request['status'] in terminal_states:
+                break
+            with yaspin(text='Request status: {}'.format(oracle_request['status'])):
+                time.sleep(10)
+        if oracle_request['status'] not in terminal_states:
+            raise RbsOracleCommonError(
+                "\nTimeout: Async request status has been {0} for longer than the timeout period of {1} minutes. The request will remain active (current status: {0})  and the script will exit.".format(
+                    oracle_request['status'], timeout))
+        else:
+            return oracle_request
+
+    def oracle_db_rename(self, oracle_sid, oracle_home, new_oracle_name):
+        os.environ["ORACLE_HOME"] = oracle_home
+        os.environ["ORACLE_SID"] = oracle_sid
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'shutdown immediate'))
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'startup mount'))
+        # Change the database name
+        logfile = oracle_home + '/dbs/nid_' + new_oracle_name + '.log'
+        self.logger.info("NID Logfile: {}".format(logfile))
+        session = Popen(['nid', 'target=/', 'dbname={}'.format(new_oracle_name), 'logfile={}'.format(logfile)],
+                        stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = session.communicate()
+        self.logger.info(stdout.decode())
+        # Create an init file from the spfile
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'create pfile from spfile;'))
+        # Rename the init file
+        os.rename("{}/dbs/init{}.ora".format(oracle_home, oracle_sid),
+                  "{}/dbs/init{}.ora".format(oracle_home, new_oracle_name))
+        # Rename the password file if present
+        if os.path.exists("{}/dbs/orapw{}".format(oracle_home, oracle_sid)):
+            os.rename("{}/dbs/orapw{}".format(oracle_home, oracle_sid),
+                      "{}/dbs/orapw{}".format(oracle_home, new_oracle_name))
+        # Rename the control files
+        os.rename("{}/dbs/{}_control1".format(oracle_home, oracle_sid),
+                  "{}/dbs/{}_control1".format(oracle_home, new_oracle_name))
+        os.rename("{}/dbs/{}_control2".format(oracle_home, oracle_sid),
+                  "{}/dbs/{}_control2".format(oracle_home, new_oracle_name))
+        # Change the database name in all the parameters in the init file
+        status = subprocess.check_output(
+            "sed -i 's/{0}/{1}/g' {2}/dbs/init{1}.ora".format(oracle_sid, new_oracle_name, oracle_home), shell=True)
+        self.logger.info(status.decode())
+        # Switch the environment to the new database name
+        os.environ["ORACLE_SID"] = new_oracle_name
+        # Open the database
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'create spfile from pfile;'))
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'startup mount;'))
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'alter database open resetlogs;'))
+        return
+
+    def oracle_db_clone_cleanup(self, oracle_sid, oracle_home):
+        os.environ["ORACLE_HOME"] = oracle_home
+        os.environ["ORACLE_SID"] = oracle_sid
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'shutdown abort;'))
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'startup force mount exclusive restrict;'))
+        self.logger.info(self.sqlplus_sysdba(oracle_home, 'drop database;'))
+        self.delete_dbs_files(oracle_home, 'arch*')
+        self.delete_dbs_files(oracle_home, 'c-*')
+        self.delete_dbs_files(oracle_home, 'hc_{}.dat'.format(oracle_sid))
+        self.delete_dbs_files(oracle_home, 'init{}.ora'.format(oracle_sid))
+        self.delete_dbs_files(oracle_home, 'init{}.ora'.format(oracle_sid))
+        self.delete_dbs_files(oracle_home, 'lkO{}'.format(oracle_sid))
+        return
+
+    def sqlplus_sysdba(self, oracle_home, sql_command):
+        self.logger.info("SQL: {}".format(sql_command))
+        sql_args = [os.path.join(oracle_home, 'bin', 'sqlplus'), '-S', '/', 'as', 'sysdba']
+        session = Popen(sql_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        session.stdin.write(sql_command.encode())
+        stdout, stderr = session.communicate()
+        return stdout.decode()
+
+    def rman(self, oracle_home, rman_command):
+        self.logger.info("RMAN: {}".format(rman_command))
+        sql_args = [os.path.join(oracle_home, 'bin', 'rman'), 'target', '/']
+        session = Popen(sql_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        session.stdin.write(rman_command.encode())
+        stdout, stderr = session.communicate()
+        return stdout.decode()
+
+    def delete_dbs_files(self, oracle_home, pattern):
+        file_name = glob.glob(oracle_home + '/dbs/' + pattern)
+        for file_path in file_name:
+            try:
+                os.remove(file_path)
+            except:
+                self.logger.warning("Error while deleting file: {}".format(file_path))
+
+    @staticmethod
+    def get_latest_autobackup(path):
+        """
+        Finds the latest control file backup recursively in a path.
+
+       Args:
+            path (str): The path in which to search for a control file.
+        Returns:
+            control file backup (str): The path of the latest control file backup
+
+        """
+        file_name = glob.glob(path + '/**/controlfile_c-*', recursive=True)
+        if file_name:
+            latest = file_name[0]
+            for file_path in file_name:
+                if int(file_path[-2:], base=16) > int(latest[-2:], base=16):
+                    latest = file_path
+            return latest
+        else:
+            raise RbsOracleCommonError("No control file backups were found in {}.".format(path))
 
     @staticmethod
     def is_ip(hostname):
@@ -240,37 +436,14 @@ class RubrikRbsOracleDatabase:
             datetime_object = cluster_timezone.localize(datetime.datetime.fromisoformat(iso_time_string))
         return int(datetime_object.timestamp()) * 1000
 
-    # @staticmethod
-    # def cluster_time(iso_time_string, timezone):
-    #     """
-    #     Converts a time string in a timezone to a user friendly string in that time zone.
-    #
-    #     Args:
-    #         iso_time_string (str): Time string.
-    #         timezone (str): Time zone.
-    #     Returns:
-    #         time_string (str): Time string converted to the supplied time zone.
-    #     """
-    #     cluster_timezone = pytz.timezone(timezone)
-    #     utc = pytz.utc
-    #     if iso_time_string.endswith('Z'):
-    #         time_string = iso_time_string[:-1]
-    #         datetime_object = utc.localize(datetime.datetime.fromisoformat(iso_time_string))
-    #     else:
-    #         datetime_object = cluster_timezone.localize(datetime.datetime.fromisoformat(iso_time_string))
-    #     cluster_time_object = cluster_timezone.normalize(datetime_object.astimezone(cluster_timezone))
-    #     return cluster_time_object.isoformat()
-
     @staticmethod
     def cluster_time(time_string, timezone):
         """
         Converts a time string in a timezone to a user friendly string in that time zone.
 
-    \b
         Args:
             time_string (str): Time string.
             timezone (str): Time zone.
-    \b
         Returns:
             time_string (str): Time string converted to the supplied time zone.
         """
@@ -284,3 +457,80 @@ class RubrikRbsOracleDatabase:
         cluster_time_object = cluster_timezone.normalize(datetime_object.astimezone(cluster_timezone))
         return cluster_time_object.isoformat()
 
+
+class RubrikRbsOracleMount(RubrikRbsOracleDatabase):
+    """
+    Rubrik RBS (snappable) Oracle backup object.
+    """
+    def __init__(self, rubrik, source_database_name, source_database_host,  database_mount_host):
+        super().__init__(rubrik, source_database_name, source_database_host)
+        self.logger = logging.getLogger(__name__ + '.RubrikRbsOracleMount')
+        self.rubrik = rubrik
+        self.oracle_id = self.get_oracle_db_id()
+        self.database_mount_host = database_mount_host
+
+    def get_oracle_live_mount_id(self):
+        """
+        This will search for and retrieve the live mount id for a live mount of the database on the host.
+
+        Args:
+            self (object): Database Object
+        Returns:
+            live_mount_id (str): The id of the requested live mount.
+        """
+        oracle_live_mounts = self.rubrik.connection.get('internal', '/oracle/db/mount?source_database_id={}'.format(self.oracle_id))
+        live_mount_id = []
+        # On CDM 5.1.1+ the targetHostID id is the host or cluster name first.
+        for mount in oracle_live_mounts['data']:
+            if self.database_mount_host in mount['targetHostId']:
+                live_mount_id.append(mount['id'])
+        if live_mount_id:
+            return live_mount_id
+        # If no match the CDM release is pre 5.1.1 and we much find the id for the target host
+        # Check if host_cluster is a RAC Cluster or a node in a RAC cluster so we can use the RAC cluster id
+        rac_id = self.rubrik.connection.get('internal', '/oracle/rac?name={}'.format(self.database_mount_host))
+        mount_host_id = ''
+        if rac_id['total'] == 0:
+            rac_info = self.rubrik.connection.get('internal', '/oracle/rac')
+            for rac in rac_info['data']:
+                for nodes in rac['nodes']:
+                    if nodes['nodeName'] == self.database_mount_host:
+                        mount_host_id = rac['id']
+                if mount_host_id:
+                    break
+        elif rac_id['total'] == 1:
+            mount_host_id = rac_id['data'][0]['id']
+        else:
+            for rac in rac_id['data']:
+                if rac['primaryClusterId'] == self.rubrik.cluster_id and rac['status'] == 'Connected':
+                    mount_host_id = rac['id']
+        if not mount_host_id:
+            mount_host_id = self.get_host_id(self.rubrik.cluster_id, self.database_mount_host)
+        host_id = mount_host_id.split(':::')[1]
+        for mount in oracle_live_mounts['data']:
+            if host_id == mount['targetHostId']:
+                live_mount_id.append(mount['id'])
+                self.logger.info("Live mount id: {}".format(live_mount_id))
+        return live_mount_id
+
+    def get_live_mount_info(self, live_mount_id):
+        """
+        Gets all the information about the live mount using the live mount id.
+        """
+        live_mount_info = self.rubrik.connection.get('internal', '/oracle/db/mount/{}'.format(live_mount_id))
+        return live_mount_info
+
+    def live_mount_delete(self, live_mount_id, force):
+        """
+        This will unmount a live mounted database or backup set.
+
+        Args:
+            self (object): Database Object
+            live_mount_id (str): The id of the mount to remove,
+            force (bool): Set to true to force the unmount.
+
+        Returns:
+            live_mount_delete_info (dict): The information returned from the Rubrik CDM about the requested unmount.
+        """
+        live_mount_delete_info = self.rubrik.connection.delete('internal', '/oracle/db/mount/{}?force={}'.format(live_mount_id, force))
+        return live_mount_delete_info
