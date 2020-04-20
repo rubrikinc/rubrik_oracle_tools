34818b3a03a8262cc47d786deda59e3fe470dc3e
pcrouleur <vendors@zgoda.com>
2020-04-20 12:20:57 -0600

    Added requests async wait and select ids.

--- rubrik_oracle_unmount.py
+++ rubrik_oracle_unmount.py
@@ -8,8 +8,11 @@ import sys
 @click.option('--source_host_db', '-s', type=str, required=True,  help='The source <host or RAC cluster>:<database>')
 @click.option('--mounted_host', '-m', type=str, required=True,  help='The host with the live mount to remove')
 @click.option('--force', '-f', is_flag=True, help='Force unmount')
+@click.option('--all_mounts', '-a', is_flag=True, help='Unmount all mounts from the source host:db. Provide all the clone names separated by commas.')
+@click.option('--id_unmount', '-i', help='Unmount a specific mount using the mount id. Multiple ids seperated by commas. ')
+@click.option('--no_wait', is_flag=True, help='Queue Live Mount and exit.')
 @click.option('--debug_level', '-d', type=str, default='WARNING', help='Logging level: DEBUG, INFO, WARNING or CRITICAL.')
-def cli(source_host_db, mounted_host, force, debug_level):
+def cli(source_host_db, mounted_host, force, all_mounts, id_unmount, no_wait,  debug_level):
     """
     This will unmount a Rubrik live mount using the database name and the live mount host.
 
@@ -29,18 +32,62 @@ def cli(source_host_db, mounted_host, force, debug_level):
     logger.addHandler(ch)
 
     rubrik = rbs_oracle_common.RubrikConnection()
+    if id_unmount:
+        id_unmount = id_unmount.split(",")
+    else:
+        id_unmount = []
     source_host_db = source_host_db.split(":")
+    logger.info("Checking for live mounts of source db {} on host {}".format(source_host_db[1], mounted_host))
     mount = rbs_oracle_common.RubrikRbsOracleMount(rubrik, source_host_db[1], source_host_db[0], mounted_host)
     live_mount_ids = mount.get_oracle_live_mount_id()
+    unmount_info = []
     if not live_mount_ids:
         raise RubrikOracleUnmountError("No live mounts found for {} live mounted on {}. ".format(source_host_db[1], mounted_host))
-    else:
-        unmount_info = []
+    elif len(live_mount_ids) == 1:
+        logger.info("Found live mount id: {} on {}".format(live_mount_ids[0], mounted_host))
+        logger.info("Deleting 1 live mount.")
+        delete_request = mount.live_mount_delete(live_mount_ids[0], force)
+        if no_wait:
+            logger.warning("Live mount id: {} Unmount status: {}.".format(live_mount_ids[0], delete_request['status']))
+        else:
+            delete_request = mount.async_requests_wait(delete_request['id'], 12)
+            logger.warning("Async request completed with status: {}".format(delete_request['status']))
+            logger.debug(delete_request)
+        unmount_info.append(delete_request)
+    elif len(live_mount_ids) > 1 and all_mounts:
+        logger.info("Delete all mounts is set to {}. Deleting all mounts on {}".format(all_mounts, mounted_host))
         for live_mount_id in live_mount_ids:
-            unmount_response = mount.live_mount_delete(live_mount_id, force)
-            logger.warning("Live mount id: {} Unmount status: {}.".format(live_mount_id, unmount_response['status']))
-            unmount_info.append(unmount_response)
-        return unmount_info
+            logger.debug(live_mount_id)
+            logger.warning("Deleting live mount with id: {} on {}".format(live_mount_ids[0], mounted_host))
+            delete_request = mount.live_mount_delete(live_mount_id, force)
+            if no_wait:
+                logger.warning(
+                    "Live mount id: {} Unmount status: {}.".format(live_mount_ids[0], delete_request['status']))
+            else:
+                delete_request = mount.async_requests_wait(delete_request['id'], 12)
+                logger.warning("Async request completed with status: {}".format(delete_request['status']))
+                logger.debug(delete_request)
+            unmount_info.append(delete_request)
+    elif len(live_mount_ids) > 1 and id_unmount:
+        logger.info("Will delete the following mounts: {} on {}".format(id_unmount, mounted_host))
+        for live_mount_id in live_mount_ids:
+            if live_mount_id in id_unmount:
+                logger.debug(live_mount_id)
+                logger.warning("Deleting live mount with id: {} on {}".format(live_mount_ids[0], mounted_host))
+                delete_request = mount.live_mount_delete(live_mount_id, force)
+                if no_wait:
+                    logger.warning(
+                        "Live mount id: {} Unmount status: {}.".format(live_mount_ids[0], delete_request['status']))
+                else:
+                    delete_request = mount.async_requests_wait(delete_request['id'], 12)
+                    logger.warning("Async request completed with status: {}".format(delete_request['status']))
+                    logger.debug(delete_request)
+                unmount_info.append(delete_request)
+    else:
+        raise RubrikOracleUnmountError( "Multiple live mounts found for {} live mounted on {}. "
+                                            "Use --all_mounts or --id_mounts to unmount all or some of the mounts "
+                                            .format(source_host_db[1], mounted_host))
+    return unmount_info
 
 
 class RubrikOracleUnmountError(rbs_oracle_common.NoTraceBackWithLineNumber):
