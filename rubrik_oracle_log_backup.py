1216c28b1d75230cb8bd9e5479c0dbde53e330b3
pcrouleur <vendors@zgoda.com>
2020-04-20 15:59:22 -0600

    Switched to rbs_oracle_common, logging, and wait options.

--- rubrik_oracle_log_backup.py
+++ rubrik_oracle_log_backup.py
@@ -1,38 +1,63 @@
-import rubrik_oracle_module as rbk
+import rbs_oracle_common
 import click
-import pytz
+import logging
+import sys
 import datetime
+import pytz
 
 
 @click.command()
-@click.argument('host_cluster_db')
-def cli(host_cluster_db):
+@click.option('--source_host_db', '-s', type=str, required=True,  help='The source <host or RAC cluster>:<database>')
+@click.option('--wait', is_flag=True, help='Wait for backup to complete.')
+@click.option('--debug_level', '-d', type=str, default='WARNING', help='Logging level: DEBUG, INFO, WARNING, ERROR or CRITICAL.')
+def cli(source_host_db, wait, debug_level):
     """
     This will initiate an on demand archive log backup of the database.
 
 \b
-    The source database is specified in a host:db format.
-\b
-    Args:
-        host_cluster_db (str): The hostname the database is running on : The database name.
-\b
     Returns:
         log_backup_info (dict): The information about the snapshot returned from the Rubrik CDM.
     """
-    rubrik = rbk.connect_rubrik()
-    cluster_info = rbk.get_cluster_info(rubrik)
-    timezone = cluster_info['timezone']['timezone']
-    print("Connected to cluster: {}, version: {}, Timezone: {}.".format(cluster_info['name'], cluster_info['version'], timezone))
-    host_cluster_db = host_cluster_db.split(":")
-    oracle_db_id = rbk.get_oracle_db_id(rubrik, host_cluster_db[1], host_cluster_db[0])
-    oracle_log_backup_info = rbk.oracle_log_backup(rubrik, oracle_db_id)
-    cluster_timezone = pytz.timezone(timezone)
-    utc = pytz.utc
-    start_time = utc.localize(datetime.datetime.fromisoformat(oracle_log_backup_info['startTime'][:-1])).astimezone(
-        cluster_timezone)
-    fmt = '%Y-%m-%d %H:%M:%S %Z'
-    print("Oracle Log Backup {} \nStatus: {}, Started at {}.".format(oracle_log_backup_info['id'], oracle_log_backup_info['status'], start_time.strftime(fmt)))
+    numeric_level = getattr(logging, debug_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(debug_level))
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(numeric_level)
+    console_formatter = logging.Formatter('%(asctime)s: %(message)s')
+    ch.setFormatter(console_formatter)
+    logger.addHandler(ch)
+
+    rubrik = rbs_oracle_common.RubrikConnection()
+    source_host_db = source_host_db.split(":")
+    database = rbs_oracle_common.RubrikRbsOracleDatabase(rubrik, source_host_db[1], source_host_db[0])
+    oracle_log_backup_info = database.oracle_log_backup()
+    logging.debug(oracle_log_backup_info)
+    if wait:
+        logger.warning("Starting archive log backup of database {} on {}".format(source_host_db[1], source_host_db[0]))
+        oracle_log_backup_info = database.async_requests_wait(oracle_log_backup_info['id'], 12)
+        logger.warning("Async request completed with status: {}".format(oracle_log_backup_info['status']))
+        if oracle_log_backup_info['status'] != "SUCCEEDED":
+            raise RubrikOracleLogBackupError(
+                "Database backup (snapshot) did not complete successfully. Mount ended with status {}".format(
+                    oracle_log_backup_info['status']))
+        logger.warning("Archive log backup completed.")
+    else:
+        cluster_timezone = pytz.timezone(rubrik.timezone)
+        utc = pytz.utc
+        start_time = utc.localize(datetime.datetime.fromisoformat(oracle_log_backup_info['startTime'][:-1])).astimezone(
+            cluster_timezone)
+        fmt = '%Y-%m-%d %H:%M:%S %Z'
+        print("Oracle Log Backup {} \nStatus: {}, Started at {}.".format(oracle_log_backup_info['id'], oracle_log_backup_info['status'], start_time.strftime(fmt)))
     return oracle_log_backup_info
+
+
+class RubrikOracleLogBackupError(rbs_oracle_common.NoTraceBackWithLineNumber):
+    """
+        Renames object so error is named with calling script
+    """
+    pass
 
 
 if __name__ == "__main__":
--- rubrik_oracle_snapshot.py
+++ rubrik_oracle_snapshot.py
@@ -1,14 +1,18 @@
-import rubrik_oracle_module as rbk
+import rbs_oracle_common
 import click
-import pytz
+import logging
+import sys
 import datetime
+import pytz
 
 
 @click.command()
-@click.argument('host_cluster_db')
+@click.option('--source_host_db', '-s', type=str, required=True,  help='The source <host or RAC cluster>:<database>')
 @click.option('--force', '-f', is_flag=True, help='Force a new full database image level 0 backup')
-@click.option('--sla', '-s', type=str, help='Rubrik SLA Domain to use if different than the assigned SLA')
-def cli(host_cluster_db, force, sla):
+@click.option('--sla', type=str, help='Rubrik SLA Domain to use if different than the assigned SLA')
+@click.option('--wait', is_flag=True, help='Wait for backup to complete.')
+@click.option('--debug_level', '-d', type=str, default='WARNING', help='Logging level: DEBUG, INFO, WARNING, ERROR or CRITICAL.')
+def cli(source_host_db, force, sla, wait, debug_level):
     """
     This will initiate an on demand snapshot (backup) of the database.
 
@@ -16,34 +20,56 @@ def cli(host_cluster_db, force, sla):
     The source database is specified in a host:db format. To force a new full level 0
     image backup of the database set force to True. If you would like to use a different SLA for this snapshot you
     can specify that here also. Note if no SLA is supplied the current sla for this database will be used.
-\b
-    Args:
-        host_cluster_db (str): The hostname the database is running on : The database name.
-        force (bool): Force a new full database image level 0 backup
-        sla (str): The Rubrik SLA Domain to use if different than the assigned SLA
+
 \b
     Returns:
         snapshot_info (dict): The information about the snapshot returned from the Rubrik CDM.
     """
-    rubrik = rbk.connect_rubrik()
-    cluster_info = rbk.get_cluster_info(rubrik)
-    timezone = cluster_info['timezone']['timezone']
-    print("Connected to cluster: {}, version: {}, Timezone: {}.".format(cluster_info['name'], cluster_info['version'], timezone))
-    host_cluster_db = host_cluster_db.split(":")
-    oracle_db_id = rbk.get_oracle_db_id(rubrik, host_cluster_db[1], host_cluster_db[0])
+    numeric_level = getattr(logging, debug_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(debug_level))
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(numeric_level)
+    console_formatter = logging.Formatter('%(asctime)s: %(message)s')
+    ch.setFormatter(console_formatter)
+    logger.addHandler(ch)
+
+    rubrik = rbs_oracle_common.RubrikConnection()
+    source_host_db = source_host_db.split(":")
+    database = rbs_oracle_common.RubrikRbsOracleDatabase(rubrik, source_host_db[1], source_host_db[0])
     if sla:
-        oracle_db_sla_id = rbk.get_sla_id(rubrik, sla)
+        oracle_db_sla_id = database.get_sla_id(sla)
     else:
-        oracle_db_info = rbk.get_oracle_db_info(rubrik, oracle_db_id)
+        oracle_db_info = database.get_oracle_db_info()
         oracle_db_sla_id = oracle_db_info['effectiveSlaDomainId']
-    oracle_snapshot_info = rbk.oracle_db_snapshot(rubrik, oracle_db_id, oracle_db_sla_id, force)
-    cluster_timezone = pytz.timezone(timezone)
-    utc = pytz.utc
-    start_time = utc.localize(datetime.datetime.fromisoformat(oracle_snapshot_info['startTime'][:-1])).astimezone(
-        cluster_timezone)
-    fmt = '%Y-%m-%d %H:%M:%S %Z'
-    print("Oracle Database snapshot {} \nStatus: {}, Started at {}.".format(oracle_snapshot_info['id'], oracle_snapshot_info['status'], start_time.strftime(fmt)))
+    oracle_snapshot_info = database.oracle_db_snapshot(oracle_db_sla_id, force)
+    logging.debug(oracle_snapshot_info)
+    if wait:
+        logger.warning("Starting backup (snapshot) of database {} on {}".format(source_host_db[1], source_host_db[0]))
+        oracle_snapshot_info = database.async_requests_wait(oracle_snapshot_info['id'], 12)
+        logger.warning("Async request completed with status: {}".format(oracle_snapshot_info['status']))
+        if oracle_snapshot_info['status'] != "SUCCEEDED":
+            raise RubrikOracleSnapshotError(
+                "Database backup (snapshot) did not complete successfully. Mount ended with status {}".format(
+                    oracle_snapshot_info['status']))
+        logger.warning("Database backup (snapshot) completed.")
+    else:
+        cluster_timezone = pytz.timezone(rubrik.timezone)
+        utc = pytz.utc
+        start_time = utc.localize(datetime.datetime.fromisoformat(oracle_snapshot_info['startTime'][:-1])).astimezone(
+            cluster_timezone)
+        fmt = '%Y-%m-%d %H:%M:%S %Z'
+        logging.warning("Oracle Database snapshot {} \nStatus: {}, Started at {}.".format(oracle_snapshot_info['id'], oracle_snapshot_info['status'], start_time.strftime(fmt)))
     return oracle_snapshot_info
+
+
+class RubrikOracleSnapshotError(rbs_oracle_common.NoTraceBackWithLineNumber):
+    """
+        Renames object so error is named with calling script
+    """
+    pass
 
 
 if __name__ == "__main__":
