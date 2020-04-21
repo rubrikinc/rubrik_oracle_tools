af76beee270db33da095c8e4bea7b8a95d63e60a
pcrouleur <vendors@zgoda.com>
2020-04-21 09:10:22 -0600

    Added rubrik_oracle_mount_info.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     py_modules=['rubrik_oracle_module', 'rubrik_oracle_backup_info', 'rubrik_oracle_backup_mount',
                 'rubrik_oracle_unmount', 'rubrik_oracle_db_mount', 'rubrik_oracle_snapshot',
                 'rubrik_oracle_log_backup', 'rubrik_oracle_db_mount_clone', 'rubrik_oracle_clone_unmount',
-                'rubrik_oracle_backup_mount_clone'],
+                'rubrik_oracle_backup_mount_clone', 'rubrik_oracle_mount_info'],
     install_requires=[
         'requests >= 2.18.4, != 2.22.0',
         'rubrik_cdm',
@@ -26,5 +26,6 @@ setup(
         rubrik_oracle_db_mount_clone=rubrik_oracle_db_mount_clone:cli
         rubrik_oracle_clone_unmount=rubrik_oracle_clone_unmount:cli
         rubrik_oracle_backup_mount_clone=rubrik_oracle_backup_mount_clone:cli
+        rubrik_oracle_mount_info=rubrik_oracle_mount_info:cli
     '''
 )
