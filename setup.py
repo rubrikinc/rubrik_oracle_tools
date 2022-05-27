c5937325716a37fdeb61838b44f86d807f52bacc
Julian Zgoda <julian.zgoda@rubrik.com>
2022-05-27 13:01:19 -0600

    Added managed protection script.

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
                 'rubrik_oracle_unmount', 'rubrik_oracle_db_mount', 'rubrik_oracle_snapshot',
                 'rubrik_oracle_log_backup', 'rubrik_oracle_db_mount_clone', 'rubrik_oracle_clone_unmount',
                 'rubrik_oracle_backup_mount_clone', 'rubrik_oracle_mount_info', 'rubrik_oracle_backup_clone',
-                'rubrik_oracle_backup_validate', 'rubrik_oracle_db_clone', 'rubrik_oracle_rbs_refresh'],
+                'rubrik_oracle_backup_validate', 'rubrik_oracle_db_clone', 'rubrik_oracle_rbs_refresh',
+                'rubrik_oracle_manage_protection'],
     install_requires=[
         'requests >= 2.18.4, != 2.22.0',
         'rubrik_cdm',
@@ -33,5 +34,6 @@ setup(
         rubrik_oracle_backup_validate=rubrik_oracle_backup_validate:cli
         rubrik_oracle_db_clone=rubrik_oracle_db_clone:cli
         rubrik_oracle_rbs_refresh=rubrik_oracle_rbs_refresh:cli
+        rubrik_oracle_rbs_refresh=rubrik_oracle_manage_protection:cli
     '''
 )
