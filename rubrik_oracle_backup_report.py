a8e5d1c5ac72ccdc19dc72f6c6c713c7e5c2ac3c
Julian Zgoda <julian.zgoda@rubrik.com>
2022-10-19 12:08:34 -0600

    Continued refinements.

--- rubrik_oracle_backup_report.py
+++ rubrik_oracle_backup_report.py
@@ -6,9 +6,8 @@ import rbs_oracle_common
 
 
 @click.command()
-@click.option('--source_host_db', '-s', type=str, required=False,  help='The source <host or RAC cluster>:<database>')
 @click.option('--debug_level', '-d', type=str, default='WARNING', help='Logging level: DEBUG, INFO, WARNING or CRITICAL.')
-def cli(source_host_db, debug_level):
+def cli(debug_level):
     """
     Displays information about the Oracle database object, the available snapshots, and recovery ranges.
     If no source_host_db is supplied, all non-relic Oracle databases will be listed.
@@ -24,80 +23,67 @@ def cli(source_host_db, debug_level):
     console_formatter = logging.Formatter('%(asctime)s: %(message)s')
     ch.setFormatter(console_formatter)
     logger.addHandler(ch)
-
+    t = rbs_oracle_common.Timer(text="RBS Connection took {:0.2f} seconds", logger=logging.debug)
+    t.start()
     rubrik = rbs_oracle_common.RubrikConnection()
-    if source_host_db:
-        print("*" * 95)
-        print("Connected to cluster: {}, version: {}, Timezone: {}.".format(rubrik.name, rubrik.version, rubrik.timezone))
-        source_host_db = source_host_db.split(":")
-        database = rbs_oracle_common.RubrikRbsOracleDatabase(rubrik, source_host_db[1], source_host_db[0])
-        oracle_db_info = database.get_oracle_db_info()
-        print("*" * 95)
-        if 'dataGuardType' in oracle_db_info.keys():
-            if oracle_db_info['dataGuardType'] == 'DataGuardGroup':
-                print("Data Guard Group Details ")
-                print("Data Guard Group Name: {0}   ID: {1}".format(oracle_db_info['name'], oracle_db_info['id']))
+    t.stop()
+    print("*" * 110)
+    print("Connected to cluster: {}, version: {}, Timezone: {}.".format(rubrik.name, rubrik.version, rubrik.timezone))
+    t = rbs_oracle_common.Timer(text="Database list GET took {:0.2f} seconds", logger=logging.debug)
+    t.start()
+    databases = rubrik.connection.get("internal", "/oracle/db")
+    t.stop()
+    db_data = []
+    db_headers = ["Host/Cluster", "Database", "DG_Group", "SLA", "Log Freq", "Last DB BKUP", "Last LOG BKUP", "Missed"]
+    for db in databases['data']:
+        db_element = [''] * 8
+        dg_id = False
+        if not db['isRelic']:
+            if 'standaloneHostName' in db.keys():
+                db_element[0] = db['standaloneHostName']
+            elif 'racName' in db.keys():
+                db_element[0] = db['racName']
+            db_element[1] = db['sid']
+            if 'dataGuardType' in db.keys():
+                if db['dataGuardType'] == 'DataGuardMember':
+                    db_element[2] = db['dataGuardGroupName'].split('DG_GROUP_')[1]
+                    dg_id = db['dataGuardGroupId']
+                    dg_groups = rubrik.connection.get("v1", "/oracle/db/{0}".format(db['dataGuardGroupId']))
+                    db_element[3] = dg_groups['effectiveSlaDomainName']
+                else:
+                    db_element[2] = 'None'
+                    db_element[3] = db['effectiveSlaDomainName']
+            if 'logBackupFrequencyInMinutes' in db.keys():
+                db_element[4] = db['logBackupFrequencyInMinutes']
             else:
-                print("Database Details ")
-                print("Database name: {0}   ID: {1}".format(oracle_db_info['name'], oracle_db_info['id']))
-        if 'standaloneHostName' in oracle_db_info.keys():
-            print("Host Name: {}".format(oracle_db_info['standaloneHostName']))
-        elif 'racName' in oracle_db_info.keys():
-            print("Rac Cluster Name: {}    Instances: {}".format(oracle_db_info['racName'], oracle_db_info['numInstances']))
-        if 'dataGuardType' in oracle_db_info.keys():
-            if oracle_db_info['dataGuardType'] == 'DataGuardGroup':
-                for member in oracle_db_info['dataGuardGroupMembers']:
-                    print("DB Unique Name: {0}    Host: {1}    Role: {2}".format(member['dbUniqueName'], member['standaloneHostName'], member['role']))
-        print("SLA: {}    Log Backup Frequency: {} min.    Log Retention: {} hrs.".format(oracle_db_info['effectiveSlaDomainName'], oracle_db_info['logBackupFrequencyInMinutes'], oracle_db_info['logRetentionHours']))
-        oracle_snapshot_info = database.get_oracle_db_snapshots()
-        logger.debug(oracle_snapshot_info)
-        print("*" * 95)
-        print("Available Database Backups (Snapshots):")
-        for snap in oracle_snapshot_info['data']:
-            print("Database Backup Date: {}   Snapshot ID: {}".format(database.cluster_time(snap['date'], rubrik.timezone)[:-6], snap['id']))
-
-        oracle_db_recoverable_range_info = database.get_oracle_db_recoverable_range()
-        print("*" * 95)
-        print("Recoverable ranges:")
-        for recovery_range in oracle_db_recoverable_range_info['data']:
-            print("Begin Time: {}   End Time: {}".format(database.cluster_time(recovery_range['beginTime'], rubrik.timezone)[:-6],
-                                                         database.cluster_time(recovery_range['endTime'], rubrik.timezone)[:-6]))
-    else:
-        print("*" * 110)
-        print("Connected to cluster: {}, version: {}, Timezone: {}.".format(rubrik.name, rubrik.version, rubrik.timezone))
-        databases = rubrik.connection.get("internal", "/oracle/db")
-        db_data = []
-        db_headers = ["Host/Cluster", "Database", "DG_Group", "SLA", "Log Freq", "Last DB BKUP", "Missed"]
-        for db in databases['data']:
-            db_element = [''] * 7
-            if not db['isRelic']:
-                if 'standaloneHostName' in db.keys():
-                    # db_element.append(db['standaloneHostName'])
-                    db_element[0] = db['standaloneHostName']
-                elif 'racName' in db.keys():
-                    db_element[0] = db['racName']
-                db_element[1] = db['sid']
-                if 'dataGuardType' in db.keys():
-                    if db['dataGuardType'] == 'DataGuardMember':
-                        db_element[2] = db['dataGuardGroupName'].split('DG_GROUP_')[1]
-                        dg_groups = rubrik.connection.get("v1", "/oracle/db/{0}".format(db['dataGuardGroupId']))
-                        db_element[3] = dg_groups['effectiveSlaDomainName']
-                    else:
-                        db_element[2] = 'None'
-                        db_element[3] = db['effectiveSlaDomainName']
-                if 'logBackupFrequencyInMinutes' in db.keys():
-                    db_element[4] = db['logBackupFrequencyInMinutes']
-                else:
-                    db_element[4] = "None"
-                if 'lastSnapshotTime' in db.keys():
-                    db_element[5] = db['lastSnapshotTime'][:-5]
-                else:
-                    db_element[5] = "None"
-                db_element[6] = db['numMissedSnapshot']
-                db_data.append(db_element)
-        db_data.sort(key=lambda x: (x[0], x[1]))
-        print("*" * 110)
-        print(tabulate(db_data, headers=db_headers))
+                db_element[4] = "None"
+            if 'lastSnapshotTime' in db.keys():
+                db_element[5] = db['lastSnapshotTime'][:-5]
+            else:
+                db_element[5] = "None"
+            db_element[7] = db['numMissedSnapshot']
+            # t = rbs_oracle_common.Timer(text="Database details GET took {:0.2f} seconds", logger=logging.debug)
+            # t.start()
+            # database = rbs_oracle_common.RubrikRbsOracleDatabase(rubrik, db_element[1], db_element[0])
+            # oracle_db_info1 = database.get_oracle_db_info()
+            # t.stop()
+            if dg_id:
+                id = dg_id
+            else:
+                id = db['id']
+            t = rbs_oracle_common.Timer(text="Database details direct GET took {:0.2f} seconds", logger=logging.debug)
+            t.start()
+            oracle_db_details = rubrik.connection.get("v1", "/oracle/db/{0}".format(id))
+            t.stop()
+            if 'latestRecoveryPoint' in oracle_db_details.keys():
+                # db_element[6] = format(rbs_oracle_common.cluster_time(oracle_db_details['latestRecoveryPoint'], rubrik.timezone)[:-6])
+                db_element[6] = oracle_db_details['latestRecoveryPoint']
+            else:
+                db_element[6] = "None"
+            db_data.append(db_element)
+    db_data.sort(key=lambda x: (x[0], x[1]))
+    print("*" * 110)
+    print(tabulate(db_data, headers=db_headers))
 
 
 class RubrikOracleBackupInfoError(rbs_oracle_common.NoTraceBackWithLineNumber):
